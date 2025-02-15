d4ea79e8d82924b07a466a5819ad51be53dbb21b
letram055 <tramb2105564@student.ctu.edu.vn>
2025-02-15 23:07:55 +0700

    trang tam

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,20 @@
+@extends('admin/layouts/master')
+
+@section('title')
+Home
+@endsection
+
+@section('feature-title')
+Home
+@endsection
+
+@section('content')
+<div>
+    <h1>Home</h1>
+    <p>Chào mừng bạn đến với trang quản trị của Hope Cafe</p>
+    <p>Đây là trang quản trị hệ thống của quán cafe Hope Cafe</p>
+    <p>Chúc bạn có một ngày làm việc hi
+</div>
+
+
+@endsection
