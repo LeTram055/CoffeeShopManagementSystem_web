913e444f04a3dcd40546178cace710946cd17e6e
letram055 <tramb2105564@student.ctu.edu.vn>
2025-04-11 00:20:01 +0700

    cap nhat thong bao

--- resources/views/staff_baristas/ingredient/index.blade.php
+++ resources/views/staff_baristas/ingredient/index.blade.php
@@ -72,6 +72,20 @@ Nguyên liệu
     @endforeach
 </div>
 
+<div class="toast-container position-fixed top-0 end-0 p-3">
+    <div id="orderToast" class="toast align-items-center text-white bg-success border-0" role="alert"
+        aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+            <div class="toast-body">
+
+            </div>
+
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                aria-label="Close"></button>
+        </div>
+    </div>
+</div>
+
 <div class="container-search mt-3">
     <input type="text" id="searchInput" class="form-control" placeholder="Tìm kiếm nguyên liệu...">
 </div>
